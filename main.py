c5950f8105c44f2f685b0c9d1b37111dce21899f
Parsa Mahmoodi <parsa.mahmoodi81@gmail.com>
2025-11-12 06:35:21 +0400

    added prompt for running command to fix error. | catching exceptions | fixed url var | added keys used in url

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ import logging
 from playwright.async_api import async_playwright, Error
 from bs4 import BeautifulSoup
 
+import getpass
 from prompt_toolkit import prompt
-from prompt_toolkit.shortcuts import yes_no_dialog
 
+"""
 system = sys.platform
 if system.startswith("win"):
     chromedriver = "./chromedriver-win64/chromedriver.exe"
@@ -23,11 +24,12 @@ else:
     except OSError as e:
         print(e)
         exit(1)
+"""
 
-
+keys = ("MC_MX", "USDOT")
 carriers = []
 soup = None
-url = f"https://safer.fmcsa.dot.gov/query.asp?query_type=queryCarrierSnapshot&query_param=MC_MX&query_string=1714408"
+url = f"https://safer.fmcsa.dot.gov/query.asp?query_type=queryCarrierSnapshot&query_param={key}&query_string={id}"
 
 
 def read_file():
@@ -38,30 +40,57 @@ def read_file():
 
 
 async def fetch_page():
-    try:
-        async with async_playwright() as p:
-            browser = await p.chromium.launch()
-            page = await browser.new_page()
-            await page.wait_for_load_state("networkidle")
-            html = await page.content()
-            # Optional: parse HTML with BeautifulSoup
-            soup = BeautifulSoup(html, "html.parser")
-            print(soup.prettify())
-            await browser.close()
-    except Error as e:
-        if "playwright install" in str(e):
-            print("Error: Playwright has no browsers installed")
-            
-            subprocess.run(["playwright", "install"], check=True)
-            # p
+    while True:
+        try:
+            async with async_playwright() as p:
+                browser = await p.chromium.launch()
+                page = await browser.new_page()
+                await page.wait_for_load_state("networkidle")
+                html = await page.content()
+                # Optional: parse HTML with BeautifulSoup
+                soup = BeautifulSoup(html, "html.parser")
+                print(soup.prettify())
+                await browser.close()
+        except Error as e:
+            if "playwright install" in str(e):
+                print("Error: Playwright has no browsers installed")
+                # time.sleep(1)
+                answer = yes_no_dialog(
+                    "Do you want this script to run 'playwright install' command"
+                )
+                if answer:
+                    try:
+                        subprocess.run(["playwright", "install"], check=True)
+                    except subprocess.CalledProcessError as er:
+                        print(f"Installation failed with exit code {er.returncode}")
+                        print(f"Command that failed: {er.cmd}")
+                        sys.exit(1)
+                    continue
+                else:
+                    print("Exiting Program")
+                    sys.exit(1)
+
+
+def yes_no_dialog(qstr: str) -> bool:
+    """ "Ask the user for a YES or NO question. Returns True/False"""
+    while True:
+        answer = prompt(
+            message=qstr,
+            default=f"{getpass.getuser().strip().lower()}",
+        )
+        if answer in ["y", "yes"]:
+            return True
+        elif answer in ["n", "no"]:
+            return False
+        else:
+            print("Please only type y/yes or n/no")
 
 
 def find():
-    ...
+    re.search()
 
 
-def main():
-    ...
+def main(): ...
 
 
 if __name__ == "__main__":
